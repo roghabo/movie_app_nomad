9df31bcc53c779a2d886b0fbada69bfe1eeabe4b
roghabo <roghabo@gmail.com>
2020-04-27 17:22:26 +0900

    Updates

---
+++ precache-manifest.513d759c665f7df31218b106ccb1fa96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6788b3279d6e7fdae7d6d6167354f68a",
+    "url": "/movie_app_nomad/index.html"
+  },
+  {
+    "revision": "3e793288c5d6fdd452f6",
+    "url": "/movie_app_nomad/static/css/main.9df05f8c.chunk.css"
+  },
+  {
+    "revision": "bc0c81660faa2d015d10",
+    "url": "/movie_app_nomad/static/js/2.f9b189aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_nomad/static/js/2.f9b189aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e793288c5d6fdd452f6",
+    "url": "/movie_app_nomad/static/js/main.9f8cba09.chunk.js"
+  },
+  {
+    "revision": "3837794460e219df321c",
+    "url": "/movie_app_nomad/static/js/runtime-main.14466ec3.js"
+  }
+]);
